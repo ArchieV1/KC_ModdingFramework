835b8e85cbaf803697e7666a44b586079624295a
ArchieV1 <archie.j.vann@gmail.com>
2022-09-17 02:35:34 +0100

    UNTESTED: Implemented StoneLikeGenerator Added empty `Tools` subclasses Added GetPrivateField from a type not just an object (For static)

--- KCModdingFramework/ModdingFrameworkAPI.cs
+++ KCModdingFramework/ModdingFrameworkAPI.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text;
+using Assets.Code;
 using Harmony;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
@@ -270,8 +271,53 @@ public class GeneratorBase
 /// </summary>
 public class StoneLikeGenerator : GeneratorBase
 {
-    public StoneLikeGenerator(ResourceTypeBase[] resourceTypeBases) : base(resourceTypeBases)
+    private ResourceTypeBase[] _resourceTypeBases;
+    private bool _largeFeature = false;
+    private int[] _mapSizeBiases;
+    
+    private struct MapFeatureDef
     {
+        // Taken from `World.MapFeatureDef` though feature doesn't seem needed
+        // public World.MapFeature feature; 
+        public int x;
+        public int z;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="resourceTypeBases"></param>
+    /// <param name="largeFeature"></param>
+    /// <param name="ironMode">If true changes mapSizeBiases default to follow the numbers for iron instead of stone. No effect with custom mapSizeBiases</param>
+    /// <param name="mapSizeBiases">How many extra resource deposits to place per map size. Small, Medium, Large. Default {1, 2, 4}</param>
+    public StoneLikeGenerator(ResourceTypeBase[] resourceTypeBases, bool largeFeature=false, bool ironMode=false, int[] mapSizeBiases=null) : base(resourceTypeBases)
+    {
+        // Assumes each ResourceTypeBase will not be malformed
+        if (resourceTypeBases.Length == 2)
+        {
+            _resourceTypeBases = resourceTypeBases;
+        }
+        else
+        {
+            _resourceTypeBases = new ResourceTypeBase[]
+            {
+                resourceTypeBases[0], new ResourceTypeBase
+                {
+                    ResourceType = ResourceType.UnusableStone
+                }
+            };
+        }
+
+        _largeFeature = largeFeature;
+        
+        if (mapSizeBiases != null && mapSizeBiases.Length == 3)
+        {
+            _mapSizeBiases = mapSizeBiases;
+        }
+        else
+        {
+            _mapSizeBiases = ironMode ? new[] {1, 1, 2} : new[] {1, 2, 4};
+        }
     }
 
     /// <summary>
@@ -282,32 +328,119 @@ public class StoneLikeGenerator : GeneratorBase
     /// <returns></returns>
     public override bool Generate(World world)
     {
-        if (Resources.Length <= 2)
+        // Edited version of `TryPlaceResource`: 2022/09/14
+        // Recreating values of params
+        // private void TryPlaceResource(World.MapFeature feature, ArrayExt<Cell> cellsToLandmassFiltered, List<World.MapFeatureDef> placedFeatures)
+        
+        // feature
+        // The "feature" (Iron deposit, Fertile ground etc) to place
+        // Not needed
+        
+        // placedFeatures
+        // A list of a currently placed features. Seems to be empty each time it is given to `TryPlaceResource` though.
+        // So the game only makes sure resources of the same type arent near each other? Different types can spawn next to each other?
+        List<MapFeatureDef> placedFeatures = new List<MapFeatureDef>();
+        
+
+        // cellsToLandmassFiltered
+        // ?? A list of cells comprising each island?
+        ArrayExt<Cell>[] array = new ArrayExt<Cell>[world.NumLandMasses];
+        for (int k = 0; k < world.NumLandMasses; k++)
         {
-            return false;
+            Cell[] cellData = (Cell[]) Tools.GetPrivateWorldField(world, "cellData", fieldIsStatic: false);
+            array[k] = new ArrayExt<Cell>(cellData.Length);
+            for (int l = 0; l < world.cellsToLandmass[k].Count; l++)
+            {
+                if (world.cellsToLandmass[k].data[l].Type != ResourceType.Water)
+                {
+                    array[k].Add(world.cellsToLandmass[k].data[l]);
+                }
+            }
         }
-        // Do StoneLike generation
+        
+        // For each island
+        for (int num3 = 0; num3 < world.NumLandMasses; num3++)
+        {
+            ArrayExt<Cell> cellsToLandmassFiltered = array[num3];
+            
+            // Make different amounts of resources generate based on map size
+            int mapSizeBias = 0;
+            if (world.generatedMapsBias == World.MapBias.Land)
+            {
+                if (world.generatedMapSize == World.MapSize.Small)
+                {
+                    mapSizeBias = _mapSizeBiases[0];
+                }
+                else if (world.generatedMapSize == World.MapSize.Medium)
+                {
+                    mapSizeBias = _mapSizeBiases[1];
+                }
+                else if (world.generatedMapSize == World.MapSize.Large)
+                {
+                    mapSizeBias = _mapSizeBiases[2];
+                }
+            }
 
+            int index2 = 0;  // Seems to just always be 0
+            for (int depositNumber = 0; depositNumber < world.baseDensities[index2].stone + mapSizeBias; depositNumber++)
+            {
+                // this.TryPlaceResource(World.MapFeature.SmallStone, array[num3], placedFeatures);
+                
+                // Edited implementation of the above method. Does not have `placedFeatures` as that is not stored anywhere
+                bool flag = false;
+                int num = 50;
+                while (!flag && num > 0)
+                {
+                    Cell cell = cellsToLandmassFiltered.RandomElement();
+                    bool flag2 = false;
+                    for (int i = 0; i < placedFeatures.Count; i++)
+                    {
+                        float xCoordDelta = Mathff.Abs(placedFeatures[i].x - cell.x);
+                        float zCoordDelta = Mathff.Abs(placedFeatures[i].z - cell.z);
+                        if (xCoordDelta < 4f && zCoordDelta < 4f)
+                        {
+                            flag2 = true;
+                            break;
+                        }
+                    }
 
-        return base.Generate(world);
+                    if (!flag2)
+                    {
+                        switch (_largeFeature)
+                        {
+                            case true:
+                                flag = Tools.PlaceLargeStoneFeature(world, cell, _resourceTypeBases[0].ResourceType, _resourceTypeBases[1].ResourceType, 60, 35);
+                                break;
+                            default:
+                                flag = Tools.PlaceSmallStoneFeature(world, cell, _resourceTypeBases[0].ResourceType, _resourceTypeBases[0].ResourceType, 60, 35);
+                                break;
+                        }
+
+                        if (flag)
+                        {
+                            placedFeatures.Add(new MapFeatureDef
+                            {
+                                //feature = feature,
+                                x = cell.x,
+                                z = cell.z
+                            });
+                        }
+                    }
+
+                    num--;
+                }
+            }
+        }
+
+        return true;  // It worked. TODO Make this actually reflect if it worked or not
     }
+
+    
 }
 
 public class WoodLikeGenerator : GeneratorBase
 {
     public WoodLikeGenerator(ResourceTypeBase[] resourceTypeBases) : base(resourceTypeBases)
-    {
-    }
-
-    public override bool Generate(World world)
-    {
-        return base.Generate(world);
-    }
-}
-
-public class IronLikeGenerator : GeneratorBase
-{
-    public IronLikeGenerator(ResourceTypeBase[] resourceTypeBases) : base(resourceTypeBases)
     {
     }
 
@@ -377,6 +510,21 @@ public class PlacementFailedException : Exception
 
 public static class Tools
 {
+    public static class WorldEditor
+    {
+        
+    }
+
+    public static class JSON
+    {
+        
+    }
+
+    public static class Private
+    {
+        
+    }
+    
     public static Cell[] GetCellData(World world)
     {
         return (Cell[]) GetPrivateField(world, "cellData");
@@ -435,13 +583,13 @@ public static class Tools
     }
 
     /// <summary>
-    /// Get value of a private field
+    /// Get value of a private field from an Instance
     /// </summary>
     /// <param name="instance">The instance that contains the private field</param>
     /// <param name="fieldName">The private field name</param>
     /// <param name="fieldIsStatic">Is the field static</param>
     /// <returns></returns>
-    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="ArgumentException">Thrown when fieldName is not found in instance</exception>
     public static object GetPrivateField(object instance, string fieldName, bool fieldIsStatic = false)
     {
         string exceptionString =
@@ -456,6 +604,37 @@ public static class Tools
             if (fieldInfo == null) throw new ArgumentException(exceptionString);
             
             result = fieldInfo.GetValue(instance);
+        }
+        catch (Exception e)
+        {
+            ULogger.Log(e);
+            throw new ArgumentException(exceptionString);
+        }
+
+        return result;
+    }
+    
+    /// <summary>
+    /// Get value of a private field from Static Class
+    /// </summary>
+    /// <param name="type">The Class that contains the private field</param>
+    /// <param name="fieldName">The private field name</param>
+    /// <param name="fieldIsStatic">Is the field static</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException">Thrown when fieldName is not found in type</exception>
+    public static object GetPrivateField(Type type, string fieldName, bool fieldIsStatic = false)
+    {
+        string exceptionString =
+            $"{fieldName} does not correspond to a private {(fieldIsStatic ? "static" : "instance")} field in {type}";
+        object result;
+
+        try
+        {
+            FieldInfo fieldInfo = fieldIsStatic ? type.GetField(fieldName, BindingFlags.Static | BindingFlags.NonPublic) : type.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            
+            if (fieldInfo == null) throw new ArgumentException(exceptionString);
+            
+            result = fieldInfo.GetValue(type);
         }
         catch (Exception e)
         {
@@ -489,6 +668,118 @@ public static class Tools
         return string.Join(", ", GetCallingMethods().Where(u => u != "GetCallingMethodsAsString"));
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="world"></param>
+    /// <param name="cell"></param>
+    /// <param name="resourceType"></param>
+    /// <param name="chance1">Chance of growing the stoneGrowList</param>
+    /// <param name="chance2">Chance of placing secondary resource after chance1. Chance of placing secondary resource is chance1 * chance2 per adjacent tile (4)y</param>
+    /// <param name="secondaryResourceType"></param>
+    /// <returns></returns>
+    public static bool PlaceLargeStoneFeature(World world, Cell cell, ResourceType resourceType, ResourceType secondaryResourceType=ResourceType.UnusableStone, int chance1=60, int chance2=35)
+    {
+        bool flag = false;
+        flag |= PlaceSmallStoneFeature(world, cell, resourceType);
+        if (!flag)
+        {
+            return false;
+        }
+        int num = SRand.Range(1, 2);
+        for (int i = 0; i < num; i++)
+        {
+            int num2 = SRand.Range(2, 3);
+            int num3 = (SRand.Range(0, 100) > 50) ? 1 : -1;
+            int num4 = (SRand.Range(0, 100) > 50) ? 1 : -1;
+            Cell cell2 = world.GetCellData(cell.x + num2 * num3, cell.z + num2 * num4);
+            PlaceSmallStoneFeature(world, cell2, resourceType, secondaryResourceType, chance1, chance2);
+        }
+        return flag;
+    }
+
+    public static bool PlaceSmallStoneFeature(World world, Cell cell, ResourceType resourceType, ResourceType secondaryResourceType=ResourceType.UnusableStone, int chance1=60, int chance2=35)
+    {
+        Cell[] scratch4 = new Cell[4];
+        world.GetNeighborCells(cell, ref scratch4);
+        for (int i = 0; i < 4; i++)
+        {
+            if (scratch4[i] != null && scratch4[i].Type == ResourceType.Water)
+            {
+                return false;
+            }
+        }
+
+        // If first thing it does it clear the list it cant be that important? It is only used in this method and when it is defined with World.World() as so
+        List<Cell> stoneGrowList = new List<Cell>();  
+        stoneGrowList.Clear();
+        
+        bool result = world.PlaceStone(cell, resourceType);
+        Vector3[] array = new Vector3[]
+        {
+            new Vector3(-1f, 0f, 0f),
+            new Vector3(1f, 0f, 0f),
+            new Vector3(0f, 0f, -1f),
+            new Vector3(0f, 0f, 1f)
+        };
+        stoneGrowList.Add(cell);
+        while (stoneGrowList.Count > 0)
+        {
+            int x = stoneGrowList[0].x;
+            int z = stoneGrowList[0].z;
+            stoneGrowList.RemoveAt(0);
+            
+            // If the zeroth position of stoneGrowList is the same as the Cell being passed num3=2 else num3=3
+            // Num3 (+1) is the number of UnusableStone to place
+            // First run (When placing around Resource) this WILL be true (3 stones)
+            // Subsequent runs (When placing around an UnusableStone (21% chance) or adjacent to previous placement) will place up to 4 stones
+            
+            // End results:
+            // In each direction:
+            // 21% chance of placing UnusableStone + Adding that UnusableStoneCell to stoneGrowList
+            // 60% chance add adding that UnusableStoneCell to stoneGrowList
+            
+            // Runs max 20 times cus of the `num -= 3;` 
+            
+            // This leads to the possible behaviour of a resource being placed and then nothing else for 20 tiles then 4 rocks around a point
+            // The chance of this happening is:
+            // ( {[3*0.40]*[1*(0.60*0.65)]} * {[3*0.43]*[1*(0.57*0.65)]} ... {[3*0.97]*[1*(0.03*0.65)]} ) * [4*0.03]
+            // ( PROD_SUM (x=0.03, lim 0.6): [3*(0.40+x)]*[(0.60-x)*0.65] ) * [4*0.03]
+            // ( PROD_SUM (x=1, lim 20): 1.95(0.40+x*0.03)(0.60-x*0.03) ) * [4*0.03]
+            // = 1.29914E-10 * [4*0.04]
+            // = 1.55879E-11
+            // 1 in 100 000 000 000 
+            // 1 in 100 billion chance
+            // ....................U..
+            // R..................U.U.
+            // ....................U..
+            int num3 = (x == cell.x && z == cell.z) ? 2 : 3;
+            for (int j = 0; j < array.Length; j++)  // array.Length == 4
+            {
+                if (SRand.Range(0, 100) < chance1)  // 60% chance
+                {
+                    if (SRand.Range(0, 100) < chance2)  // 35% chance  ==> 21% chance of placing a stone. FOR EACH DIRECTION
+                    {
+                        Cell cell2 = world.GetCellData(x + (int)array[j].x, z + (int)array[j].z);
+                        world.PlaceStone(cell2, secondaryResourceType);
+                        num3--;
+                    }
+                    // This stops the for loop if placed all (2 : 3) stone.
+                    // With how this is written it is not 2 or 3 stone it is 3 or 4 stones
+                    if (num3 <= 0)
+                    {
+                        break;
+                    }
+                    // Add the unusable stone coords to the stone grow list
+                    stoneGrowList.Add(world.GetCellData(x + (int)array[j].x, z + (int)array[j].z));
+                }
+            }
+            // Lowers the first % chance by 3%. Max 20 runs in the while loop
+            chance1 -= 3;
+        }
+        return result;
+    }
+    
     public static bool IsEncodable(object obj)
     {
         try
